b67edfcd03a8fe383001bea6d9370c2fb563fa0e
debarros <pj.debarros@gmail.com>
2017-08-17 14:28:27 -0400

    Fixed VbetterComp so that the second parameter can be a single element, which is then compared against each element of the first parameter

--- R/VbetterComp.R
+++ R/VbetterComp.R
@@ -1,7 +1,7 @@
 #' @title Vector Better Comparison
 #' @description Compare two vectors element by element
 #' @param x A numeric vector
-#' @param y A numeric vector of the same length as \code{x}
+#' @param y A numeric vector of the same length as \code{x} or of length 1 (if y has length 1, it will be repeated to the length of x)
 #' @return A logical vector where each position indicates whether the values in those positions of the input vectors are equal.  NAs are considered equal.
 #' @examples
 #' ex1 = 6:10
@@ -14,9 +14,15 @@
 #' VbetterComp(ex2, ex3)
 #' VbetterComp(ex2, ex4)
 #' VbetterComp(ex3, ex4)
+#' VbetterComp(ex3, NA)
+#' VbetterComp(ex3, 10)
 VbetterComp = function(x,y){
   if(length(x) != length(y)){
-    stop("Vectors must be of equal length")
-  }
+    if(length(y) == 1){
+      y = rep(y, times = length(x))
+    } else {
+      stop("Vectors must be of equal length")
+    } # /if-else
+  } # /if
   apply(X = cbind(x,y), MARGIN = 1, FUN = betterComp)
-}
+} # /VbetterComp
