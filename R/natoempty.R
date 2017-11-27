f48da464db4669947f1e4cc1c637580c017cd5cf
debarros <pj.debarros@gmail.com>
2017-11-27 17:56:11 -0500

    added the Data Frame NA to Empty function DFna.to.empty

--- R/natoempty.R
+++ R/natoempty.R
@@ -16,3 +16,32 @@ na.to.empty = function(x){
 
 
 
+#' @title Data Frame NA to Empty
+#' @description Converts all NA's in the character and/or factor columns of a data.frame vector to empty values
+#' @param x A data.frame
+#' @param coltypes Character of length 1 indicating what types of columns to affect.  Acceptable values are "character", "factor", and "both".
+#' @return A data.frame with the same dimensions and names as \code{x}
+#' @examples
+#' V1 = c(LETTERS[1:8], NA, NA)
+#' V2 = c(NA, 1:8, NA)
+#' V3 = factor(c(NA,rep(c("A","B"), times = 4),NA))
+#' ex1 = data.frame(V1 = V1, V2 = V2, V3 = V3, stringsAsFactors = F)
+#' DFna.to.empty(ex1)
+DFna.to.empty = function(x, coltypes = "both"){
+  for(i in 1:ncol(x)){                # for each column
+    y = unlist(x[,i])                 # get a vector of the current column
+    if(is.character(y)){              # if the column is character
+      y = na.to.empty(y)              # run na.to.empty
+    } else if(is.factor(y)){          # if y is a factor variable
+      yLevels = levels(y)             # grab the levels of y
+      if(any(is.na(y))){              # if y has any NA's
+        if(!("" %in% yLevels)){       # if y needs a blank level
+          levels(y) = c(yLevels, "")  # add a blank level to y
+        } # /if y needs a blank level
+        y[which(is.na(y))] = ""       # make NA's blank
+      }
+    } # /if y is factor
+    x[,i] = y                       # load the values back in the data.frame
+  } # /for each column
+  return(x)                         # return the data.frame
+} # /function
