d951a527ed046d7aed25d91dbd9c33bafd50b71a
debarros <pj.debarros@gmail.com>
2018-12-13 19:52:18 -0500

    Added more detail to the documentation for the writeSIRS function

--- R/writeSIRS.R
+++ R/writeSIRS.R
@@ -1,7 +1,7 @@
-#' @title Write SIRS Extract
-#' @description Write a csv file in the manner needed for SIRS extracts
-#' @param x the object to be written, preferably a matrix or data frame. If not, it is attempted to coerce x to a data frame.
-#' @param filename a character string naming a file
+#' @title Write Level 0 SIRS Extract
+#' @description Write a csv file in the manner needed for importing into Level 0
+#' @param x the object to be written, preferably a matrix or data frame. If not, it is attempted to coerce \code{x} to a data frame.
+#' @param filename a character string naming a file.
 #' @return nothing is returned
 write.SIRS = function(x, filename){
   write.table(x = x,
