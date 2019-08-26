f8ab36724800124377188f853da1e1744d57c89e
debarros <pj.debarros@gmail.com>
2019-08-26 13:15:41 -0400

    Added the type parameter to the write.psimport function

--- R/WritePSimport.R
+++ R/WritePSimport.R
@@ -4,9 +4,10 @@
 #' @description This is a wrapper for write.table that produces a file formatted to be imported into PowerSchool
 #' @param x the object to be written, preferably a matrix or data frame. If not, it is attempted to coerce x to a data frame.
 #' @param file a character string naming a file
+#' @param type either "quick" (if using the quick import function) or "manager" (if using the data import manager).  Defaults to "quick".
 #' @param messageLevel integer of length 1 indicating the level of messaging to print
 #' @return Nothing
-write.psimport <- function (x, file, messageLevel = 0) {
+write.psimport <- function (x, file, type = "quick", messageLevel = 0) {
 
   # Error Handling
   if(!(is.matrix(x) | is.data.frame(x))){
@@ -20,9 +21,16 @@ write.psimport <- function (x, file, messageLevel = 0) {
   if(length(file) != 1){
     stop("file must have length 1")
   }
+  if(type == "quick"){
+    eol = "\r\n"
+  } else if(type == "manager"){
+    eol = "\n"
+  } else {
+    stop("type must be either 'quick' or 'manager'")
+  }
   # /Error Handling
 
-  write.table(x = x, file = file, quote = F, sep = "\t", eol = "\r\n", na = "", row.names = F)
+  write.table(x = x, file = file, quote = F, sep = "\t", eol = eol, na = "", row.names = F)
 
 } # /function
 
