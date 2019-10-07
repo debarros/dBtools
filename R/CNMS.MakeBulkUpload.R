b228f415a76fed71131beb74a39433fd9aceb8ea
debarros <pj.debarros@gmail.com>
2019-10-07 16:13:31 -0400

    In the CNMS.MakeBulkUpload function, added in a bunch of other variables.  Also added in some error handling stuff to look at what variables are expected vs what's in the import file.  This will eventually develop into something much more extensive, allowing for variability in columns names and whatnot.

--- R/CNMS.MakeBulkUpload.R
+++ R/CNMS.MakeBulkUpload.R
@@ -1,3 +1,6 @@
+# singleGender = "M"
+# xlsxFile = file.choose()
+
 #' @title Make Bulk Upload for NYSSIS/Child Nutrition Management System
 #' @description Based on an export from the Student Management System, generate a text file that can be uploaded to NYSSIS (using the CN role) to generate matches to SNAP and Medicaid records for direct certification of free lunch eligibility
 #' @param xlsxFile An xlsx file, Workbook object, or URL to an xlsx file
@@ -13,8 +16,8 @@ CNMS.MakeBulkUpload = function(xlsxFile,
                                messageLevel = 0){
 
   # Read in the student data export from PowerSchool
-  CheckStuData = read.xlsx(xlsxFile = xlsxFile)
-  CheckStuData$DOB = xlDate(CheckStuData$DOB)
+  CheckStuData = openxlsx::read.xlsx(xlsxFile = xlsxFile)
+  CheckStuData$DOB = xlDate(x = CheckStuData$DOB)
   if(!is.null(singleGender)){
     if(singleGender %in% c("M", "F")){
       CheckStuData$Gender = singleGender
@@ -31,24 +34,43 @@ CNMS.MakeBulkUpload = function(xlsxFile,
   CheckStuData$guardian2 = paste0(CheckStuData$StudentCoreFields.guardian_fn," ", CheckStuData$StudentCoreFields.guardian_ln)
   CheckStuData$guardian2[nchar(CheckStuData$guardian2) == 1] = ""
 
+  CheckStuData$guardian3 = paste0(CheckStuData$Guardian_FN," ", CheckStuData$Guardian_LN)
+  CheckStuData$guardian3[nchar(CheckStuData$guardian2) == 1] = ""
+
   # Define the output variables
   outputVars = c("BedsCode","StudentID","LastName","FirstName","DOB","Gender",
                  "Address1","Address2","City","State","ZIP","Phone","Guardian1","Guardian2")
 
   # Determine which variables in the input are associated with each field in the output
-  zipVars = c("U_STUDENT_ENROLLMENT_PAGE.MAILING_2_ZIP","Zip","Mailing_Zip")
-  fnVars = c("First_Name")
+  zipVars = c("U_STUDENT_ENROLLMENT_PAGE.MAILING_2_ZIP","Zip","Mailing_Zip", "2nd_mailing_zip")
+  fnVars = c("First_name")
   lnVars = "Last_Name"
   dobVars = "DOB"
-  addrVars = c("U_STUDENT_ENROLLMENT_PAGE.MAILING_2_STREET", "Mailing_Street","Street")
-  cityVars = c("Mailing_City", "City", "U_STUDENT_ENROLLMENT_PAGE.MAILING_2_CITY")
+  addrVars = c("U_STUDENT_ENROLLMENT_PAGE.MAILING_2_STREET", "Mailing_Street","Street", "2nd_mailing_street")
+  cityVars = c("Mailing_City", "City", "U_STUDENT_ENROLLMENT_PAGE.MAILING_2_CITY", "2nd_mailing_city")
   phonVars = c("StudentCoreFields.guardiandayphone", "StudentCoreFields.father_home_phone", "U_STUDENT_ENROLLMENT_PAGE.stepdayphone",
                "U_STUDENT_ENROLLMENT_PAGE.STEP_HOME_PHONE", "U_STUDENT_ENROLLMENT_PAGE.step_cell_phone", "U_STUDENT_ENROLLMENT_PAGE.mother_cell_phone",
                "U_STUDENT_ENROLLMENT_PAGE.father_cell_phone", "U_STUDENT_ENROLLMENT_PAGE.EMERG_WORK_PHONE_3", "U_STUDENT_ENROLLMENT_PAGE.EMERG_WORK_PHONE_2",
                "U_STUDENT_ENROLLMENT_PAGE.emerg_work_phone_1", "U_STUDENT_ENROLLMENT_PAGE.EMERG_CELL_PHONE_3", "U_STUDENT_ENROLLMENT_PAGE.EMERG_CELL_PHONE_2",
                "U_STUDENT_ENROLLMENT_PAGE.emerg_cell_phone_1", "StudentCoreFields.motherdayphone", "StudentCoreFields.mother_home_phone",
                "StudentCoreFields.emerg_3_phone", "Emerg_Phone_2", "Emerg_Phone_1", "Home_Phone")
-  guardVars = c("Father", "StudentCoreFields.guardian", "Mother", "guardian2")
+  guardVars = c("Father", "StudentCoreFields.guardian", "Mother", "guardian2", "guardian", "guardian3", "U_STUDENT_ENROLLMENT_PAGE.stepparent", "StepParent")
+  guardComponentColumns = c("StudentCoreFields.guardian_fn", "StudentCoreFields.guardian_ln", "Guardian_FN", "Guardian_LN")
+
+
+  columnsNotUsed = setdiff(colnames(CheckStuData), c(zipVars, fnVars, lnVars, dobVars, addrVars, cityVars, phonVars, guardVars, guardComponentColumns))
+  columnsNotFound = setdiff(c(zipVars, fnVars, lnVars, dobVars, addrVars, cityVars, phonVars, guardVars, guardComponentColumns), colnames(CheckStuData))
+
+  if(length(columnsNotUsed) > 0){
+    print("The following columns are in the import file, but will not be used:")
+    print(columnsNotUsed)
+  }
+
+  if(length(columnsNotFound) > 0){
+    print("The following columns are missing from the import file!")
+    print(columnsNotFound)
+    stop()
+  }
 
 
   # Establish a list to hold the student information in list format
