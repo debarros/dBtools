dc3327c6d4b56d39225110bad9789cf7ac055ef1
debarros <pj.debarros@gmail.com>
2020-09-16 12:27:00 -0400

    Added groupBy parameter to CharCartProd

--- R/CharCartProd.R
+++ R/CharCartProd.R
@@ -2,14 +2,24 @@
 #' @description Create a character vector of the cartesian product of two character vectors
 #' @param x A character vector
 #' @param y A character vector
+#' @param groupBy either 1 or 2.  Determines which variable gets grouped together in the results.
 #' @return A character vector where each element is a concatenation of one element from x and one element from y
 #' @examples
 #' vecOne = letters[1:5]
 #' vecTwo = LETTERS[6:8]
 #' CharCartProd(x = vecOne, y = vecTwo)
 #' CharCartProd(vecTwo, vecOne)
-CharCartProd = function(x, y){
-  stuff = expand.grid(x,y)
+#' CharCartProd(vecOne, vecTwo, 2)
+#' CharCartProd(vecTwo, vecOne, 2)
+CharCartProd = function(x, y, groupBy = 1){
+  if(groupBy == 1){
+    stuff = expand.grid(x,y)
+  } else if (groupBy == 2){
+    stuff = expand.grid(y,x)
+    stuff = stuff[,c(2,1)]
+  } else {
+    stop("groupBy must be either 1 or 2")
+  }
   stuff = apply(stuff, 1, paste0, collapse = "")
   return(stuff)
-}
+} #/function
